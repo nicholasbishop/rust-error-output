2ca488a61977e56cec684d60daea84502436dbed
Nicholas Bishop <nicholasbishop@gmail.com>
2020-09-13 14:06:22 -0400

    Implement basic rendering

---
+++ src/bin/render.rs
@@ -0,0 +1,67 @@
+use command_run::Command;
+use std::path::Path;
+use std::{fs, io};
+
+#[derive(Default)]
+struct Output {
+    lines: Vec<String>,
+}
+
+impl Output {
+    fn add_rust_block<S: AsRef<str>>(&mut self, contents: S) {
+        self.lines.push("'''rust".into());
+        self.lines
+            .extend(contents.as_ref().lines().map(|line| line.to_string()));
+        self.lines.push("'''".into());
+    }
+
+    fn add_code_block<S: AsRef<str>>(&mut self, contents: S) {
+        self.lines.push("'''".into());
+        self.lines
+            .extend(contents.as_ref().lines().map(|line| line.to_string()));
+        self.lines.push("'''".into());
+    }
+
+    fn finalize(&self) -> String {
+        self.lines.join("\n")
+    }
+
+    fn add_line(&mut self, line: &str) {
+        self.lines.push(line.into());
+    }
+}
+
+fn main() -> io::Result<()> {
+    let template = include_str!("../../template.md");
+
+    let mut output = Output::default();
+    for line in template.lines() {
+        let prefix = "!!! ";
+        if line.starts_with(prefix) {
+            let file_name = &line[prefix.len()..];
+            let path = Path::new("src/bin").join(file_name);
+
+            let contents = fs::read_to_string(path)?;
+            output.add_rust_block(contents);
+
+            let mut cmd = Command::new(
+                Path::new("target/debug").join(file_name.replace(".rs", "")),
+            );
+            cmd.check = false;
+            cmd.capture = true;
+            cmd.log_command = false;
+            let cmdout = cmd.run().unwrap();
+            if !cmdout.stdout.is_empty() {
+                panic!("unexpected stdout from {}", file_name);
+            }
+
+            output.add_code_block(cmdout.stderr_string_lossy());
+        } else {
+            output.add_line(line);
+        }
+    }
+
+    println!("{}", output.finalize());
+
+    Ok(())
+}
---
+++ template.md
@@ -0,0 +1,9 @@
+lorem ipsum blah blah blah
+
+!!! anyhow_context.rs
+
+blah blah blah
+
+!!! main_result.rs
+
+end stuff
